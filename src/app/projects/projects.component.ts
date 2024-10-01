29700aefe548d9c78fd56f564814484fe0de5910
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-10-01 09:19:19 -0500

    Added projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardActions, MatCardContent, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardActions,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatButton
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
