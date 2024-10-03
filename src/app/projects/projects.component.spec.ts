9c64134a9aa5565a393c86fff1d35a5cfca0e4ba
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-10-03 11:42:28 -0500

    Code cleaned

--- src/app/projects/projects.component.spec.ts
+++ src/app/projects/projects.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProjectsComponent } from './projects.component';
+import {ProjectsComponent} from './projects.component';
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
@@ -10,7 +10,7 @@ describe('ProjectsComponent', () => {
     await TestBed.configureTestingModule({
       imports: [ProjectsComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ProjectsComponent);
     component = fixture.componentInstance;
